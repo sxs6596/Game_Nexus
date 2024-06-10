7d10393ef52006cf455fb9119d77c3c8144eb84a
sxs6596 <sxs6596@mavs.uta.edu>
2024-06-09 22:15:51 -0500

    modified darkmode component

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -7,8 +7,8 @@ const DarkMode = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button onClick={toggleColorMode} margin={3} leftIcon={colorMode === 'light' ? <MdDarkMode /> : <CiDark />}>
-      {colorMode === 'light' ? 'Dark' : 'Light'} Mode
+    <Button onClick={toggleColorMode} margin={3} leftIcon={colorMode === 'light' ? <MdDarkMode /> : <CiDark />} borderRadius='50%' padding={0}>
+       
     </Button>
   );
 };
